ce197c7fb7fa27b7a6a8ecfc953fc18d73d01b15
Vaibhavi Jaiswal <vaibhavi.jaiswal2023@vitstudent.ac.in>
2024-09-05 19:46:49 +0530

    login and signup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,6 @@
 import { createRoot } from "react-dom/client";
+
+
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
--- frontend/src/pages/signup/Signup.js
+++ frontend/src/pages/signup/Signup.js
@@ -1,5 +1,4 @@
 import "./Signup.css";
-
 import useSignup from "../../hooks/useSignup";
 import { useState } from "react";
 
@@ -12,49 +11,57 @@ function Signup() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(email, password, confirm);
     if (password === confirm) {
       setMatch(true);
       signup(email, password);
     } else setMatch(false);
   }
-  return (
-    <form onSubmit={handleSubmit} className="signup-form">
-      <h2>Sign Up</h2>
-      <label>
-        <span>Email:</span>
-        <input
-          type="email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-      </label>
-      <label>
-        <span>Password:</span>
-        <input
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-      </label>
-      <label>
-        <span>Confirm Password:</span>
-        <input
-          type="password"
-          onChange={(e) => setConfirm(e.target.value)}
-          value={confirm}
-        />
-      </label>
 
-      {!isPending && <button className="btn">Submit</button>}
-      {isPending && (
-        <button className="btn" disabled>
-          loading
-        </button>
-      )}
-      {error && <p>{error}</p>}
-      {!match && <p>Passwords do not match</p>}
-    </form>
+  return (
+    <div className="signup-container">
+      <div className="image-container">
+        <img src="/rocket.png" alt="Rocket" />
+      </div>
+      <div className="form-container">
+        <form onSubmit={handleSubmit} className="signup-form">
+          <h2>Sign Up</h2>
+          <label>
+            <span>Email:</span>
+            <input
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+          </label>
+          <label>
+            <span>Password:</span>
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+          </label>
+          <label>
+            <span>Confirm Password:</span>
+            <input
+              type="password"
+              onChange={(e) => setConfirm(e.target.value)}
+              value={confirm}
+            />
+          </label>
+
+          {!isPending && <button className="btn">Submit</button>}
+          {isPending && (
+            <button className="btn" disabled>
+              loading
+            </button>
+          )}
+          {error && <p>{error}</p>}
+          {!match && <p>Passwords do not match</p>}
+        </form>
+      </div>
+    </div>
   );
 }
+
 export default Signup;
