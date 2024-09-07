294bb0ebbf584b9c8e009331834ead873c73040f
Aadithya-J <jlaadithya@gmail.com>
2024-09-07 21:49:57 +0530

    fix package issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -21,6 +22,12 @@ export default defineConfig({
     sourcemap: false, 
   },
   server: {
+    fs: {
+      allow: [
+        // Add the path to your project root
+        path.resolve(__dirname, '..'),
+      ],
+    },
     sourcemapIgnoreList: () => true, 
   },
 })
