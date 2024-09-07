a7d86b39713218f935bf5edd209da38456b554fc
Vaibhavi Jaiswal <vaibhavi.jaiswal2023@vitstudent.ac.in>
2024-09-07 20:38:45 +0530

    logoutfixed

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
 import useLogout from "../../hooks/useLogout";
 import { useAuthContext } from "../../hooks/useAuthContext";
-
 import "./Navbar.css";
 
 function Navbar() {
   const { logout } = useLogout();
   const { user } = useAuthContext();
+
   return (
     <nav className="navbar">
       <ul>
-        <li className="title">Yatharth</li>
+        <li>
+          {/* Make 'Yatharth' a link to the homepage */}
+          <Link to="/" className="title">Yatharth</Link>
+        </li>
         <div className="list">
           {!user && (
             <>
@@ -24,20 +27,20 @@ function Navbar() {
           )}
           {user && (
             <>
-            <li>
-              <button className="btn" onClick={logout}>
-                Log Out
-              </button>
-            </li>
-            <li>
-                <Link className="btn" to="/roadmap">roadmap</Link>
-            </li>
+              <li>
+                <button className="btn" onClick={logout}>
+                  Log Out
+                </button>
+              </li>
+              <li>
+                <Link className="btn" to="/roadmap">Roadmap</Link>
+              </li>
             </>
-
           )}
         </div>
       </ul>
     </nav>
   );
 }
+
 export default Navbar;
