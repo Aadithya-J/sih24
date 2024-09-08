340b8dafc8c4ae1220ad4f5fd1898424a2035b94
Vaibhavi Jaiswal <vaibhavi.jaiswal2023@vitstudent.ac.in>
2024-09-08 20:57:09 +0530

    navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,42 +1,79 @@
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import useLogout from "../../hooks/useLogout";
 import { useAuthContext } from "../../hooks/useAuthContext";
+import { IoMenu } from "react-icons/io5";
 import "./Navbar.css";
 
 function Navbar() {
   const { logout } = useLogout();
   const { user } = useAuthContext();
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(prev => !prev);
+  };
+
+  const closeMenuOnMobile = () => {
+    if (window.innerWidth <= 1150) {
+      setShowMenu(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 1150) {
+        setShowMenu(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <nav className="navbar">
-      <ul>
-        <li>
-          {/* Make 'Yatharth' a link to the homepage */}
-          <Link to="/" className="title">Yatharth</Link>
-        </li>
-        <div className="list">
-          {!user && (
-            <>
+      <div className="navbar__container">
+        <Link to="/" className="navbar__logo">
+          YATHARTH
+        </Link>
+        <div
+          className={`navbar__menu ${showMenu ? "show-menu" : ""}`}
+          aria-expanded={showMenu}
+        >
+          <ul className="navbar__list">
+            {!user && (
+              <>
+                <li>
+                  <Link to="/login" className="navbar__link" onClick={closeMenuOnMobile}>
+                    Login
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/signup" className="navbar__link" onClick={closeMenuOnMobile}>
+                    Sign Up
+                  </Link>
+                </li>
+              </>
+            )}
+            {user && (
               <li>
-                <Link to="/login" className="btn">Login</Link>
-              </li>
-              <li>
-                <Link to="/signup" className="btn">Sign Up</Link>
-              </li>
-            </>
-          )}
-          {user && (
-            <>
-              <li>
-                <button className="btn purple-button" onClick={logout}>
+                <button className="navbar__button" onClick={logout}>
                   Log Out
                 </button>
               </li>
-             
-            </>
-          )}
+            )}
+          </ul>
         </div>
-      </ul>
+        <div
+          className="navbar__toggle"
+          role="button"
+          aria-label="Open menu"
+          onClick={toggleMenu}
+        >
+          <IoMenu />
+        </div>
+      </div>
     </nav>
   );
 }
